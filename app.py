1614c6b148a9a73262959bf0db95ea6adc91261e
Jennifer Xu <jennxu23@stanford.edu>
2020-07-11 10:30:18 -0400

    added and linked character selector page

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-#flask stuff here hi jenn
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template('home.html') #picture won't load on my end for some reason
+
+#TODO char screen
+@app.route("/char_select")
+def char_select():
+    return render_template('char_sel.html')
+
+#TODO battle room
